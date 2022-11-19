f7536092bff182a23b7e3f8eefef496f391044d8
Michael Chop <007micle007@gmail.com>
2022-11-19 10:57:43 +0300

    Added interaction with the database

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import Post from './Post.js'
 
 const PORT = process.env.PORT || 5000
+const DB_URL = 'mongodb+srv://admin:wwwwww@cluster0.2pbhfnm.mongodb.net/test?retryWrites=true&w=majority'
 const app = express()
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.status(200).json('Bruh')
+app.post('/', async (req, res) => {
+    try {
+        const {author, title, content, picture} = req.body
+        const post = await Post.create({author, title, content, picture})
+        res.json(post)
+    } catch(err) {
+        res.status(500).json(err)
+    }
 })
 
-app.listen(PORT, (err) => {
-    if(err) {
-        console.log('Error: ', err)
+async function startApp() {
+    try {
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true})
+        app.listen(PORT, () => console.log(`Server has been started on port: ${PORT}...`))
+    } catch (err) {
+        console.log(err)
     }
-    console.log(`Server has been started on port: ${PORT}...`)
-})
+}
+
+startApp()
+
